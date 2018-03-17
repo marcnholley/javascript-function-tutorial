20b0e132b1a180a3b0b99ef005d96520457bea45
Marc <35475608+marcnholley@users.noreply.github.com>
2018-03-17 00:07:13 -0700

    Added JavaScript function tutorial files

---
+++ tutorial.js
@@ -0,0 +1,19 @@
+//Marc Holley, 04/10/16, CIS166AA 31865 , Professor Anderson*
+//global variables
+
+var a = 1;
+var b = 1;
+
+//example function
+function addingNumbers(a, b) {
+
+	result = a + b;
+	
+	return result;
+}
+
+//function result output to webpage
+document.getElementById("result").innerHTML = result;
+
+
+
